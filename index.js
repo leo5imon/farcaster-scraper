56b248cb0b723f9111d733827f962dfeb671175f
Arthur <0xa1337@proton.me>
2024-02-07 18:14:47 +0100

    feat: turn hardcoded values into cli params

--- index.js
+++ index.js
@@ -1,48 +1,106 @@
-import 'dotenv/config'
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
 
-// Configuration variables
-const API_KEY = process.env.NEYNAR_API_KEY;
-const CHANNEL_ID = 'memes';
-const LIKE_THRESHOLD = 30;
-const RECAST_THRESHOLD = 15;
-const FOLLOWER_COUNT_THRESHOLD = 20;
-const FETCH_LIMIT = 100;
-const URL_DOMAIN_FILTER = 'imgur.com';
-
-const options = {
-    method: 'GET',
-    headers: {accept: 'application/json', api_key: API_KEY}
-};
+import "dotenv/config";
 
 let allImgurUrls = [];
 
-function fetchCasts(cursor = '') {
-    const cursorParam = cursor ? `&cursor=${encodeURIComponent(cursor)}` : '';
-    const url = `https://api.neynar.com/v2/farcaster/feed?feed_type=filter&filter_type=channel_id&channel_id=${CHANNEL_ID}&with_recasts=true&with_replies=false&limit=${FETCH_LIMIT}${cursorParam}`;
-    
-    fetch(url, options)
-      .then(response => response.json())
-      .then(data => {
-          if (!data.casts || !Array.isArray(data.casts)) {
-              return;
-          }
+function fetchCasts({
+  apiKey,
+  channelId,
+  likeThreshold,
+  recastThreshold,
+  followerCountThreshold,
+  limit,
+  urlDomainFilter,
+  cursor = "",
+}) {
+  const options = {
+    method: "GET",
+    headers: { accept: "application/json", api_key: apiKey },
+  };
 
-          const casts = data.casts;
-          const imgurUrls = casts.filter(cast => {
-              return (cast.reactions.likes.length > LIKE_THRESHOLD || cast.reactions.recasts.length > RECAST_THRESHOLD) && cast.author.follower_count > FOLLOWER_COUNT_THRESHOLD;
-          }).flatMap(cast => cast.embeds ?? [])
-            .map(embed => embed.url)
-            .filter(url => url && url.includes(URL_DOMAIN_FILTER));
+  const cursorParam = cursor ? `&cursor=${encodeURIComponent(cursor)}` : "";
+  const url = `https://api.neynar.com/v2/farcaster/feed?feed_type=filter&filter_type=channel_id&channel_id=${channelId}&with_recasts=true&with_replies=false&limit=${limit}${cursorParam}`;
 
-          allImgurUrls = [...allImgurUrls, ...imgurUrls];
+  fetch(url, options)
+    .then((response) => response.json())
+    .then((data) => {
+      if (!data.casts || !Array.isArray(data.casts)) {
+        return;
+      }
 
-          if (data.next && data.next.cursor) {
-              fetchCasts(data.next.cursor);
-          } else {
-              console.log(allImgurUrls);
-          }
-      })
-      .catch(err => console.error(err));
+      const casts = data.casts;
+      const imgurUrls = casts
+        .filter((cast) => {
+          return (
+            (cast.reactions.likes.length > likeThreshold ||
+              cast.reactions.recasts.length > recastThreshold) &&
+            cast.author.follower_count > followerCountThreshold
+          );
+        })
+        .flatMap((cast) => cast.embeds ?? [])
+        .map((embed) => embed.url)
+        .filter((url) => url && url.includes(urlDomainFilter));
+
+      allImgurUrls = [...allImgurUrls, ...imgurUrls];
+
+      if (data.next && data.next.cursor) {
+        fetchCasts({ ...arguments[0], cursor: data.next.cursor });
+      } else {
+        console.log(allImgurUrls);
+      }
+    })
+    .catch((err) => console.error(err));
 }
 
-fetchCasts();
+yargs(hideBin(process.argv))
+  .command({
+    command: "fetch",
+    describe: "Fetch Imgur URLs from Neynar API based on criteria",
+    builder: {
+      channelId: {
+        default: "memes",
+        describe: "Channel ID",
+        type: "string",
+      },
+      likeThreshold: {
+        default: 30,
+        describe: "Like threshold",
+        type: "number",
+      },
+      recastThreshold: {
+        default: 15,
+        describe: "Recast threshold",
+        type: "number",
+      },
+      followerCountThreshold: {
+        default: 20,
+        describe: "Follower count threshold",
+        type: "number",
+      },
+      limit: {
+        default: 10,
+        describe: "Fetch limit",
+        type: "number",
+      },
+      urlDomainFilter: {
+        default: "imgur.com",
+        describe: "URL domain filter",
+        type: "string",
+      },
+    },
+    handler: (argv) => {
+      fetchCasts({
+        apiKey: process.env.NEYNAR_API_KEY,
+        channelId: argv.channelId,
+        likeThreshold: argv.likeThreshold,
+        recastThreshold: argv.recastThreshold,
+        followerCountThreshold: argv.followerCountThreshold,
+        limit: argv.limit,
+        urlDomainFilter: argv.urlDomainFilter,
+      });
+    },
+  })
+  .demandCommand(1, "You must provide at least one command to execute")
+  .help().argv;
